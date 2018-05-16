4d841fd823e46a5fc137028d8dd9d9a352de4529
Sonny Duque <anthondd@usc.edu>
2018-05-16 15:48:42 -0700

    Diff page: (unfinished) working version experimenting with dynamically setting multi-dimensional array.

--- ClientApp/src/app/diff-page/diff.page.component.ts
+++ ClientApp/src/app/diff-page/diff.page.component.ts
@@ -1,11 +1,140 @@
 import { Component } from '@angular/core';
 
+let shallowArray = function (a, b, compare) {
+  var l = a.length
+  if (l !== b.length) return false
+
+  if (compare) {
+    for (var i = 0; i < l; i++)
+      if (!compare(a[i], b[i])) return false
+  } else {
+    for (var i = 0; i < l; i++) {
+      if (a[i] !== b[i]) return false
+    }
+  }
+
+  return true
+}
+
+let shallowObject = function (a, b, compare) {
+
+  var ka = 0
+  var kb = 0
+
+  if (compare) {
+    for (var key in a) {
+      if (
+        a.hasOwnProperty(key) &&
+        !compare(a[key], b[key])
+      ) return false
+
+      ka++
+    }
+  } else {
+    for (var key in a) {
+      if (
+        a.hasOwnProperty(key) &&
+        a[key] !== b[key]
+      ) return false
+
+      ka++
+    }
+  }
+
+  for (var key in b) {
+    if (b.hasOwnProperty(key)) kb++
+  }
+
+  return ka === kb
+}
+
+let flat = function (type) {
+  return (
+    type !== 'function' &&
+    type !== 'object'
+  )
+}
+
+let shallow = function (a, b, compare) {
+  var aIsNull = a === null
+  var bIsNull = b === null
+
+  if (aIsNull !== bIsNull) return false
+
+  var aIsArray = Array.isArray(a)
+  var bIsArray = Array.isArray(b)
+
+  if (aIsArray !== bIsArray) return false
+
+  var aTypeof = typeof a
+  var bTypeof = typeof b
+
+  if (aTypeof !== bTypeof) return false
+  if (flat(aTypeof)) return compare
+    ? compare(a, b)
+    : a === b
+
+  return aIsArray
+    ? shallowArray(a, b, compare)
+    : shallowObject(a, b, compare)
+}
+
+enum SESS {
+  NOTHING = 0,
+  ACADTERM = 1,
+  SESSCODE = 2,
+  OWNSCHOOL = 3,
+  OWNDEPT = 4,
+  USERCONTACT = 5,
+  USEREMAIL = 6,
+  USERPHONE = 7,
+  CLASSFIRSTDAY = 8,
+  CLASSLASTDAY = 9,
+  DROPLASTDAY = 10,
+  WITHDRAWLASTDAY = 11,
+  FINALSFIRSTDAY = 12,
+  FINALSLASTDAY = 13,
+  GRADESFIRSTDAY = 14,
+  GRADESLASTDAY = 15,
+  RATETYPE = 16,
+  PERUNITAMOUNT = 17,
+  FLATRATEAMOUNT = 18,
+  UGRADUNITSMIN = 19,
+  UGRADUNITSMAX = 20,
+  GRADUNITSMIN = 21,
+  GRADUNITSMAX = 22,
+  CLASSLOCATION = 23,
+  SPECIALFEES = 24,
+  SESSIONBREAKS = 25,
+  COMMENTS = 26
+}
+
+enum DIFF {
+  SAME = 0,
+  NEW = 1,
+  CHANGED = 2,
+  DELETED = 3,
+}
+
+
 @Component({
   selector: 'diff-page',
   templateUrl: './diff.page.component.html',
 })
 
+
 export class DiffPageComponent {
+
+  diffArr: boolean[][] = [[false, false, false, false], [false, false, false, false]];
+
+  DIFF = DIFF;    // to export the DIFF enum to the HTML
+  SESS = SESS;    // to export the SESS enum to the HTML
+
+  sample() {
+    console.log('clicked');
+    this.diffArr[SESS.ACADTERM][DIFF.NEW] = true;
+  }
+
 
   public currSess = {
     sessionBreaks: [
@@ -138,5 +267,7 @@ export class DiffPageComponent {
   };
 
   showPrevious: boolean = true;
+
+  rateType: boolean = (this.currSess.rateType != this.prevSess.rateType);
 
 }
