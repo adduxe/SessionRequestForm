f897c7fcd7df46ebf75dc8b96deb2c3cc5e17f56
Sonny Duque <anthondd@usc.edu>
2018-06-01 09:11:07 -0700

    Experimenting passing value from Diff page to the header to display page title.

--- ClientApp/src/app/diff-page/diff.page.component.ts
+++ ClientApp/src/app/diff-page/diff.page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
 
 @Component({
   selector: 'diff-page',
@@ -9,7 +9,9 @@ import { Component } from '@angular/core';
 
 export class DiffPageComponent {
 
-  public pageTitle: string = "Diff Page";
+  @Output() pageTitle: string = "Diff Page";
+
+//  public pageTitle: string = "Diff Page";
 
   public hideShowPrev: string = "<<<<";
 
