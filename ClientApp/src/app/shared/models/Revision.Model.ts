3e4cccdfb580ad1c3801ac92334243ce6d1715f8
Sonny Duque <anthondd@usc.edu>
2018-07-24 15:35:11 -0700

    Request Form: hides the histor window when in data entry mode. Created a Data model for outgoing data from the form.

---
+++ ClientApp/src/app/shared/models/Revision.Model.ts
@@ -0,0 +1,112 @@
+class Action {
+
+  id: number;
+  group: string;
+  name: string;
+  comments: string;
+  createdDTM: Date;
+  revisionId: number
+
+  constructor() {
+    this.id = null;
+    this.group = null;
+    this.name = null;
+    this.comments = null;
+    this.createdDTM = null;
+    this.revisionId = null;
+  }
+
+}   // Action{}
+
+
+class Location {
+
+  id: number;
+  code: string;
+  start: Date;
+  end: Date;
+  revisionId: number;
+
+  constructor() {
+
+    this.id = null;
+    this.code = null;
+    this.start = null;
+    this.end = null;
+    this.revisionId = null;
+
+  } //constructor()
+
+}   // Location{}
+
+
+class Break {
+
+  start: Date;
+  end: Date;
+
+  constructor() {
+
+    this.start = null;
+    this.end = null;
+
+  }   // constructor()
+
+}   // Break{}
+
+
+class Fee {
+
+  id: number;
+  code: string;
+  amount: number;
+  population: string;
+  enrollment: string;
+
+  constructor() {
+
+    this.id = null;
+    this.code = null;
+    this.amount = null;
+    this.population = null;
+    this.enrollment = null;
+
+  }   // constructor()
+
+}   // Fee{}
+
+
+export class Revision {
+
+  id: number;
+  firstDayOfClass: Date;
+  lastDayOfClass: Date;
+  firstDayOfFinals: Date;
+  lastDayOfFinals: Date;
+  rateType: string;
+  ratePerUnitAmount: number;
+  otherFlatRateAmount: number;
+  otherRatePerUnit: number;
+  undergradFlatRateMin: number;
+  undergradFlatRateMax: number;
+  gradFlatRateMin: number;
+  gradFlatRateMax: number;
+  comments: string;
+  createdBy: string;
+  creatorEmail: string;
+  createdDTM: Date;
+  requestId: number;
+  actions: Action[];
+  breaks: any[];
+  locations: Location[];
+  specialFees: Fee[];
+
+  constructor() {
+
+    this.actions = [];
+    this.breaks = [];
+    this.locations = [];
+    this.specialFees = [];
+
+  }   // constructor()
+}   // Revision{}
